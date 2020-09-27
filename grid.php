0094befec76981d88178091d2840127d8045859c
perSimone <40167470+perSimone@users.noreply.github.com>
2020-09-27 16:59:33 +0200

    New website version 1.0
    First files uploaded to this repo. I uploads this to live today. It is still very much under construction. Am adding it here as a backup.

---
+++ grid.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>grid</title>
+    <?php include "./head.html" ?>
+</head>
+
+<body>
+    <?php include "./header.html" ?>
+    <?php include "./nav-menu.html" ?>
+    <main>
+        <div class="content">
+            <h1>Photo collage</h1>
+            <p>This will be a responsive photo collage with photos from my time as a freelance photographer. </p>
+            <p>Coming soon..</p>
+        </div>
+    </main>
+    <?php include "./footer.html" ?>
+    <?php include "./scripts.html" ?>
+</body>
+
+</html>
