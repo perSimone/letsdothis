398cadaba3ec2e7da0954819d3068c62da15b488
perSimone <40167470+perSimone@users.noreply.github.com>
2022-06-02 21:53:49 +0200

    Adds header and link component, basic styling and some minor changes to content and footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
 
+import Head from "next/head";
+import React from "react";
+
+import Header from "../src/components/Header";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Head>
+        <meta name="theme-color" content="#3c1742" />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ src/data.js
@@ -0,0 +1,23 @@
+import NavLink from "./components/NavLink";
+
+export const navLinks = [
+  {
+    name: <NavLink item="Contact Us" />,
+    path: "#contact",
+  },
+  { name: "Home", 
+    path: "/" 
+  },
+  {
+    name: "About Us",
+    path: "/about",
+  },
+  {
+    name: "Services",
+    path: "/services",
+  },
+  {
+    name: "Uses",
+    path: "/uses",
+  },
+];
